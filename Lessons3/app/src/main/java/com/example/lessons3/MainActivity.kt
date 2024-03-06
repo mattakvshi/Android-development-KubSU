a29a3fc843757b8f0b71afb6236a9119e34b40db
mattakvshi <mattakvshi@mttkv.dev>
2024-03-06 11:19:37 +0300

    update Lessons 3

--- Lessons3/app/src/main/java/com/example/lessons3/MainActivity.kt
+++ Lessons3/app/src/main/java/com/example/lessons3/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.lessons3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.lessons3.fragments.UniversityListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    interface Edit {
+        fun append()
+        fun update()
+        fun delete()
+    }
+
+    private var _miNewUniversity: MenuItem? =null
+    private var _miUpdateUniversity: MenuItem? =null
+    private var _miDeleteUniversity: MenuItem? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fcvMain, UniversityListFragment.getInstance())
+            .commit()
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        _miNewUniversity = menu?.findItem(R.id.miNewUniversity)
+        _miUpdateUniversity = menu?.findItem(R.id.miUpdateUniversity)
+        _miDeleteUniversity = menu?.findItem(R.id.miDeleteUniversity)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.miNewUniversity -> {
+                val fedit : Edit = UniversityListFragment.getInstance()
+                fedit.append()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
--- Lessons3/app/src/main/java/com/example/lessons3/fragments/UniversityListFragment.kt
+++ Lessons3/app/src/main/java/com/example/lessons3/fragments/UniversityListFragment.kt
@@ -1,21 +1,24 @@
 package com.example.lessons3.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.lessons3.MainActivity
 import com.example.lessons3.R
 import com.example.lessons3.data.University
 import com.example.lessons3.databinding.FragmentUniversityListBinding
 import java.lang.Exception
 import java.lang.reflect.Executable
 
-class UniversityListFragment : Fragment() {
+class UniversityListFragment : Fragment(), MainActivity.Edit {
 
     companion object {
         private var INSTANCE : UniversityListFragment? = null
@@ -82,4 +85,70 @@ class UniversityListFragment : Fragment() {
             }
     }
 
+    override fun append() {
+        newUniversity()
+    }
+
+    override fun update() {
+        updateUniversity()
+    }
+
+    override fun delete() {
+        deleteUniversity()
+    }
+
+
+    private fun newUniversity() {
+        val mDialogView =
+            LayoutInflater.from(requireContext()).inflate(R.layout.dialog_university_edit, null)
+        val inputName = mDialogView.findViewById<EditText>(R.id.edUniversityName)
+        val inputCity = mDialogView.findViewById<EditText>(R.id.edUniversityCity)
+        android.app.AlertDialog.Builder(requireContext())
+            .setTitle("Информация об университете")
+            .setView(mDialogView)
+            .setPositiveButton("добавить") { _, _ ->
+                if (inputName.text.isNotBlank() and inputCity.text.isNotBlank()) {
+                    viewModel.appendUniversity(inputName.text.toString(), inputCity.text.toString())
+                }
+            }
+            .setNegativeButton( "отмена", null)
+            .setCancelable(true)
+            .create()
+            .show()
+    }
+
+    private fun updateUniversity() {
+        val mDialogView =
+            LayoutInflater.from(requireContext()).inflate(R.layout.dialog_university_edit, null)
+        val inputName = mDialogView.findViewById<EditText>(R.id.edUniversityName)
+        val inputCity = mDialogView.findViewById<EditText>(R.id.edUniversityCity)
+        inputName.setText(viewModel.university?.name)
+        inputCity.setText(viewModel.university?.city)
+
+        android.app.AlertDialog.Builder(requireContext())
+            .setTitle(" Изменить информацию об университете")
+            .setView(mDialogView)
+            .setPositiveButton("изменить") { _, _ ->
+                if (inputName.text.isNotBlank() and inputCity.text.isNotBlank()) {
+                    viewModel.updateUniversity(inputName.text.toString(), inputCity.text.toString())
+                }
+            }
+            .setNegativeButton( "отмена", null)
+            .setCancelable(true)
+            .create()
+            .show()
+    }
+
+    private fun deleteUniversity() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Удаление!")
+            .setMessage("Вы действительно хотите удалить университет ${viewModel.university?.name ?: ""}?")
+            .setPositiveButton("ДА"){ _, _ ->
+                viewModel.deleteUniversity()
+            }
+            .setNegativeButton("НЕТ", null)
+            .setCancelable(true)
+            .create()
+            .show()
+    }
 }
