621c5a69d81a8bfb31860cf055e564f5cece48f9
mattakvshi <mattakvshi@mttkv.dev>
2024-04-02 09:37:51 +0300

    upadate University.kt, start work in rooms lib

--- Lessons3/app/src/main/java/com/example/lessons3/data/University.kt
+++ Lessons3/app/src/main/java/com/example/lessons3/data/University.kt
@@ -1,10 +1,18 @@
 package com.example.lessons3.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
 import java.util.UUID
 
 
+@Entity
+    (
+            indices = [Index("id")]
+            )
 data class University (
-    var id : UUID = UUID.randomUUID(),
-    var name : String = "",
-    var city : String = ""
+    @PrimaryKey var id : UUID = UUID.randomUUID(),
+    @ColumnInfo(name = "university_name") var name : String = "",
+    @ColumnInfo(name = "university_city")var city : String = ""
 )
