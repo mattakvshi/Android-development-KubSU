853cd590ad6b4dc989c83f837ad369d1cae39b0a
mattakvshi <mattakvshi@mttkv.dev>
2024-04-02 09:37:11 +0300

    upadate Faculty.kt, start work in rooms lib

--- Lessons3/app/src/main/java/com/example/lessons3/data/Faculty.kt
+++ Lessons3/app/src/main/java/com/example/lessons3/data/Faculty.kt
@@ -1,9 +1,27 @@
 package com.example.lessons3.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import androidx.room.util.foreignKeyCheck
 import java.util.UUID
 
+@Entity(tableName = "faculties",
+    indices = [Index("id"), Index("university_id")],
+    foreignKeys = [
+        ForeignKey(
+            entity = University::class,
+            parentColumns = ["id"],
+            childColumns = ["university_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+    )
+
 data class Faculty(
-    val id : UUID = UUID.randomUUID(),
-    var name : String = "",
-    var universityID: UUID?=null
+    @PrimaryKey val id : UUID = UUID.randomUUID(),
+    @ColumnInfo(name="faculty_name" ) var name : String = "",
+    @ColumnInfo(name="university_id" )var universityID: UUID?=null
 )
