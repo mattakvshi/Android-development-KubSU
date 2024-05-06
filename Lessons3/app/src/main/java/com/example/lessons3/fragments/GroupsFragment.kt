bf7826b9648296e07c942a67e2bdd1bec910af76
mattakvshi <mattakvshi@mttkv.dev>
2024-05-06 18:33:47 +0300

    bred kakoy-to

--- Lessons3/app/src/main/java/com/example/lessons3/fragments/GroupsFragment.kt
+++ Lessons3/app/src/main/java/com/example/lessons3/fragments/GroupsFragment.kt
@@ -6,26 +6,97 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.Group
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.lessons3.ActivityInterface
 import com.example.lessons3.R
+import com.example.lessons3.databinding.FragmentGroupsBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class GroupsFragment : Fragment() {
 
     companion object {
-        fun newInstance() = GroupsFragment()
+        private var INSTANCE: GroupsFragment? = null
+        fun getInstance(): GroupsFragment {
+            if (INSTANCE == null)
+                INSTANCE = GroupsFragment()
+            return INSTANCE ?: throw Exception("GroupsFragment не создан!")
+        }
+        fun newInstance(): GroupsFragment {
+            INSTANCE = GroupsFragment()
+            return INSTANCE!!
+        }
     }
 
-    private val viewModel: GroupsViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private lateinit var viewModel: GroupsViewModel
+    private var tabPosition: Int = 0
+    private lateinit var _binding: FragmentGroupsBinding
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_groups, container, false)
+        _binding = FragmentGroupsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[GroupsViewModel::class.java]
+        val ma = (requireActivity() as ActivityInterface)
+        ma.updateTitle("Факультет \"${viewModel.faculty?.name}\"")
+        viewModel.groupList.observe(viewLifecycleOwner) { createUI(it) }
+    }
+
+    private inner class GroupPageAdapter(fa: FragmentActivity, private val groups: List<Group>?): FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int {
+            return (groups?.size ?: 0)
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return StudentFragment.newInstance(groups!![position])
+        }
+    }
+
+    private fun createUI(groupList: List<Group>) {
+        binding.tlGroups.clearOnTabSelectedListeners()
+        binding.tlGroups.removeAllTabs()
+
+        for (i in 0 <= until < (groupList.size)) {
+            binding.tlGroups.addTab(binding.tlGroups.newTab().apply {
+                text = groupList.get(i).name
+            })
+        }
+        val adapter = GroupPageAdapter(requireActivity(), viewModel.groupList.value)
+        binding.vpStudents.adapter = adapter
+        TabLayoutMediator(binding.tlGroups, binding.vpStudents, true, true) {
+                tab, pos ->
+            tab.text = groupList.get(pos).name
+        }.attach()
+        tabPosition = 0
+        if (viewModel.group != null)
+            tabPosition = if (viewModel.getGroupListPosition >= 0)
+                viewModel.getGroupListPosition
+            else
+                0
+        viewModel.setCurrentGroup(tabPosition)
+        binding.tlGroups.selectTab(binding.tlGroups.getTabAt(tabPosition), true)
+
+        binding.tlGroups.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tabPosition = tab?.position!!
+                viewModel.setCurrentGroup(groupList[tabPosition])
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
     }
 }
--- Lessons3/app/src/main/java/com/example/lessons3/fragments/StudentsFragment.kt
+++ Lessons3/app/src/main/java/com/example/lessons3/fragments/StudentsFragment.kt
@@ -1,14 +1,40 @@
 package com.example.lessons3.fragments
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Group
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lessons3.ActivityInterface
+import com.example.lessons3.MainActivity
 import com.example.lessons3.R
+import com.example.lessons3.data.Student
+import com.example.lessons3.databinding.FragmentStudentsBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class StudentsFragment : Fragment() {
+
     private lateinit var group: Group
     companion object {
         fun newInstance(group: Group): StudentsFragment {
@@ -67,7 +93,132 @@ class StudentsFragment : Fragment() {
             parent: ViewGroup,
             viewType: Int
         ): StudentAdapter.ItemHolder {
-            TODO("Not yet implemented")
+
+            //ниже код с последней пары
+
+            val view = layoutInflater.inflate(R.layout.element_student_list, parent, false)
+            return ItemHolder(view)
+        }
+
+        override fun getItemCount(): Int = items.size
+        override fun onBindViewHolder(holder: StudentAdapter.ItemHolder, position: Int) {
+            holder.bind(viewModel.studentList.value!![position])
+        }
+        private var lastView: View? = null
+        private fun updateCurrentView(view: View) {
+            val ll = lastView?.findViewById<LinearLayout>(R.id.llStudentsButtons)
+            ll?.visibility = View.INVISIBLE
+            ll?.layoutParams = ll?.layoutParams.apply { this?.width = 1 }
+            val ib = lastView?.findViewById<ImageButton>(R.id.ibCall)
+            ib?.visibility = View.INVISIBLE
+            ib?.layoutParams = ib?.layoutParams.apply { this?.width = 1 }
+
+            lastView?.findViewById<ConstraintLayout>(R.id.clStudentElement)?.setBackgroundColor(
+                ContextCompat.getColor(requireContext(), R.color.white)
+            )
+            view.findViewById<ConstraintLayout>(R.id.clStudentElement).setBackgroundColor(
+                ContextCompat.getColor(requireContext(), R.color.my_blue)
+            )
+            lastView = view
+        }
+        private inner class ItemHolder(view: View): RecyclerView.ViewHolder(view) {
+
+            private lateinit var student: Student
+            @OptIn(DelicateCoroutinesApi::class)
+            fun bind(student: Student) {
+                this.student = student
+                if (student == ViewModel.student)
+                    updateCurrentView(itemView)
+                val tv = itemView.findViewById<TextView>(R.id.tvStudentName)
+                tv.text = student.shortName
+                val cl = itemView.findViewById<ConstraintLayout>(R.id.clStudentElement)
+                cl.setOnClickListener {
+                    viewModel.setCurrentStudent(student)
+                    updateCurrentView(itemView)
+                }
+                itemView.findViewById<ImageButton>(R.id.ibEditStudent).setOnClickListener {
+                    editStudent(student)
+                }
+
+                itemView.findViewById<ImageButton>(R.id.ibDeleteStudent).setOnClickListener {
+                    AlertDialog.Builder(requireContext())
+                        .setTitle("Удаление")
+                        .setMessage("Вы действительно хотите удалить студента ${viewModel.student?.shortName ?: ""}?")
+                        .setPositiveButton("Да") {_, _ ->
+                            viewModel.deleteStudent()
+                        }
+                        .setNegativeButton("Нет", null)
+                        .setCancelable(true)
+                        .create()
+                        .show()
+                }
+
+                val llb = itemView.findViewById<LinearLayout>(R.id.llStudentsButtons)
+                llb.visibility = View.INVISIBLE
+                llb?.layoutParams = llb?.layoutParams.apply { this?.width = 1 }
+                val ib = itemView.findViewById<ImageView>(R.id.ibCall)
+                ib.visibility = View.INVISIBLE
+                cl.setOnClickListener {
+                    cl.callOnClick()
+                    llb.visibility = View.INVISIBLE
+                    if (student.phone.isNotBlank())
+                        ib.visibility = View.VISIBLE
+                    MainScope().launch {
+                        val lp = llb?.layoutParams
+                        lp?.width = 1
+                        val ip = ib.layoutParams
+                        ip.width = 1
+                        while (lp?.width!! < 350) {
+                            lp?.width = lp?.width!! + 35
+                            llb?.layoutParams = lp
+                            ip.width = ip.width + 10
+                            if (ib.visibility == View.VISIBLE)
+                                ib.layoutParams = ip
+                            delay(50)
+                        }
+                    }
+                    true
+                }
+                itemView.findViewById<ImageButton>(R.id.ibCall).setOnClickListener {
+                    if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:${student.phone}"))
+                        startActivity(intent)
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CALL_PHONE), 2)
+                    }
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): StudentAdapter.ItemHolder {
+            val view = layoutInflater.inflate(R.layout.element_student_list, parent, false)
+            return ItemHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: StudentAdapter.ItemHolder, position: Int) {
+            holder.bind(viewModel.studentList.value!![position])
+        }
+
+        override fun getItemCount(): Int = items.size
+
+        private var lastView : View? = null
+        private fun updateCurrentView(view: View) {
+            val ll = lastView?.findViewById<LinearLayout>(R.id.llStudentButtons)
+            ll?.visibility = View.INVISIBLE
+            ll?.layoutParams = ll?.layoutParams.apply { this?.width =1 }
+            val ib = lastView?.findViewById<ImageButton>(R.id.ibCall)
+            ib?.visibility = View.INVISIBLE
+            ib?.layoutParams = ib?.layoutParams.apply { this?.width = 1 }
+
+            lastView?.findViewById<ConstraintLayout>(R.id.clStudent)?.setBackgroundColor(
+                ContextCompat.getColor(requireContext(), R.color.white))
+            view.findViewById<ConstraintLayout>(R.id.clStudent).setBackgroundColor(
+                ContextCompat.getColor(requireContext(), R.color.my_blue))
+            lastView = view
         }
     }
 }
